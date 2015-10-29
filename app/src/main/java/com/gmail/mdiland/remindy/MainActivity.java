8c71feef22219c977d1b84b1cf9e0d037a6cfa08
Max Diland <maxdiland@gmail.com>
2015-10-29 10:01:40 +0100

    begin speech recognition implementation

--- app/src/main/java/com/gmail/mdiland/remindy/MainActivity.java
+++ app/src/main/java/com/gmail/mdiland/remindy/MainActivity.java
@@ -1,17 +1,22 @@
 package com.gmail.mdiland.remindy;
 
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.gmail.mdiland.remindy.application.RemindyApplication;
 import com.gmail.mdiland.remindy.textprocessor.impl.SimpleReminderTextParser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private EditText etPreview;
     private EditText etInput;
+    private Button btnParse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initUi();
 
+//        btnParse.setOnTouchListener();
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -44,5 +50,66 @@ public class MainActivity extends AppCompatActivity {
 
         String inputText = etInput.getText().toString();
         textParser.process(inputText);
+    }
+
+    public void recognizeSpeech() {
+        SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+//        if (view == btnParse) {
+//            event.getButtonState()
+//        }
+        return false;
+    }
+
+    private class MyRecognitionListener implements RecognitionListener {
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(int i) {
+
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
     }
 }
